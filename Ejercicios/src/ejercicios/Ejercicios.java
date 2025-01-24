c42d6169c23a9b9e73c3e63fe039deac459bb85f
unknown <caldererortiz2005@gmail.com>
2025-01-24 14:17:42 +0100

    NOMEENTERO

---
+++ Ejercicios/src/ejercicios/Ejercicios.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+/**
+ *
+ * @author EAG
+ */
+public class Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        /*
+        //EJERCICIO 12
+        
+        System.out.println("Introduzca un número:");      
+        int pr = sc.nextInt();
+                
+        if(esPrimo(pr)){
+            System.out.println("Es primo");
+        }else{
+            System.out.println("No es primo");
+        }
+        */
+        //Ejercicio 1
+        System.out.println("Introduzca una lis de valores");
+        float lista[]= sc.nextFloat[];
+    }
+    //Ej 12 b)
+    /*
+    public static boolean esPrimo(int pr){
+        boolean primo = true;
+        for(int i = 2;i<pr;i++){
+            if(pr%i==0){
+                primo = false;
+            }
+        }
+        return primo;
+    }*/
+    //Ej 1 b)
+    
+}
