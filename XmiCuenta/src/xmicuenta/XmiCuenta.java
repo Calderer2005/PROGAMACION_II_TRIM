2de0488845789d91a1a43f26081daec0e3f3598e
unknown <caldererortiz2005@gmail.com>
2025-02-12 12:47:15 +0100

    pr

---
+++ XmiCuenta/src/xmicuenta/XmiCuenta.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmicuenta;
+
+/**
+ *
+ * @author EAG
+ */
+public class XmiCuenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        circunferencia c1 = new circunferencia(2.34f);
+        circunferencia c2 = new circunferencia(2.34f);
+        
+        System.out.println(c1.perimetro());
+    }
+    
+}
---
+++ XmiCuenta/src/xmicuenta/circunferencia.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmicuenta;
+
+/**
+ *
+ * @author EAG
+ */
+public class circunferencia {
+    //atributos
+    public float radio;
+    
+    //CONSTRUCTOR
+    public circunferencia(float r){
+        radio = r;
+    }
+    //METODOS
+    public float perimetro(){
+        return (float)(2*Math.PI*radio);
+    }
+    
+    public float getRadio(){
+        return radio;
+    }
+    
+    public void setradio(float r){
+        radio = r;
+    }
+}
